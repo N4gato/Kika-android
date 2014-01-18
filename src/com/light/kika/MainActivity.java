449cd2a2b0e52542ad0e6e8663d3b3d8a7da91c4
N4gato <bouayad.n4@gmail.com>
2014-01-18 10:08:44 +0000

    Some changes

--- src/com/light/kika/MainActivity.java
+++ src/com/light/kika/MainActivity.java
@@ -2,10 +2,6 @@ package com.light.kika;
 
 import android.os.Bundle;
 import android.app.Activity;
-import android.text.Editable;
-import android.text.TextWatcher;
-import android.util.Log;
-import android.view.Menu;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -14,7 +10,7 @@ import android.widget.TextView;
 public class MainActivity extends Activity {
 
 	int conter=0;
-	Button add1,add2,sub1,sub2;
+	Button add2,readd;
 	TextView tv1,tv2;
 	EditText read;
 	
@@ -23,75 +19,40 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         
-        add1 = (Button) findViewById(R.id.add1);
-        add2 = (Button) findViewById(R.id.add2);
-        sub1 = (Button) findViewById(R.id.sub1);
-        sub2 = (Button) findViewById(R.id.sub2);
+       
+        add2 = (Button) findViewById(R.id.add);
         tv1 = (TextView) findViewById(R.id.textView);
         tv2 = (TextView) findViewById(R.id.textView1);
         read = (EditText) findViewById(R.id.editText1);
-   
+        readd = (Button) findViewById(R.id.read);
         
-        add1.setOnClickListener(new View.OnClickListener() {
-			
+        
+ 		
+        add2.setOnClickListener(new View.OnClickListener() {
+       	 
 			@Override
 			public void onClick(View arg0) {
 				// TODO Auto-generated method stub
-			String	quoi = read.getText().toString();
-			conter = Integer.parseInt(quoi); 
+				
 			conter++; 
-			read.clearAnimation();
+			
 				tv1.setText("You now :" + conter);
 			}
 		});
-        
-        
-        sub1.setOnClickListener(new View.OnClickListener() {
-			
-			@Override
-			public void onClick(View arg0) {
-				// TODO Auto-generated method stub
-				//String	quoi = read.getText().toString();
-				//conter = Integer.parseInt(quoi); 
-				conter--; 
-				//read.clearAnimation();
-					tv1.setText("You now :" + conter);
-			}
-		});
-        
-        	sub2.setOnClickListener(new View.OnClickListener() {
-			
+   
+        readd.setOnClickListener(new View.OnClickListener() {
+
 			@Override
 			public void onClick(View arg0) {
 				// TODO Auto-generated method stub
 				String	quoi = read.getText().toString();
-				conter = Integer.parseInt(quoi); 
-				conter--; 
-				read.clearAnimation();
-					tv2.setText("You now :" + conter);
+				conter = Integer.parseInt(quoi);
+				tv2.setText("Thanks: You did good to enter such a number");
 			}
 		});
-        	
-        	add2.setOnClickListener(new View.OnClickListener() {
-    			
-    			@Override
-    			public void onClick(View arg0) {
-    				// TODO Auto-generated method stub
-    				String	quoi = read.getText().toString();
-    				conter = Integer.parseInt(quoi); 
-    				conter++; 
-    				read.clearAnimation();
-    					tv2.setText("You now :" + conter);
-    			}
-    		});
-    }
-
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
+        
+        
+      
     }
     
 }
